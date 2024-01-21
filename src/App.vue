06e967c03919cb47641d69f83c7316e6adc4cb9a
Javier Dominguez Galindo <joshjavy@gmail.com>
2024-01-21 16:43:43 -0600

    104. Añadiendo CSS global

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,64 @@
 
 <template>
   <div>
+    <header>
     <h1>planificador de gastos</h1>
+  </header>
+
+
+  
   </div>
   
 </template>
 
 <style>
-  
+  :root{
+    --azul: #3b82f6;
+    --blanco: #FFF;
+    --gris-claro: #f5f5f5;
+    --gris: #94a3b8;
+    --gris-oscuro: #64748b;
+    --negro:#000;
+  }
+
+  html{
+    font-size: 62.5%;
+    box-sizing: border-box;
+  }
+
+*,
+*:before,
+*::after{
+  box-sizing: inherit;
+}
+
+body{
+  font-size: 1.6rem;
+  font-size: "lato", sans-serif;
+  background-color: var(--gris-claro);
+}
+
+h1{
+  font-size:  4rem;
+}
+
+h2{
+  font-size:3rem;
+}
+
+header{
+  background-color: var(--azul);
+}
+
+header h1{
+  padding: 3rem 0;
+  margin: 0;
+  color: var(--blanco);
+  text-align: center;
+}
+
+
+
+
 
 </style>
