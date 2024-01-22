0458916dec262e44d5573fdd21cc41d28b6a85b9
Javier Dominguez Galindo <joshjavy@gmail.com>
2024-01-21 18:56:46 -0600

    105. Primeros pasos con el Formulario de Presupuesto

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <script setup>
-  
+  import Presupuesto from './components/Presupuesto.vue'
 </script>
 
 <template>
   <div>
     <header>
     <h1>planificador de gastos</h1>
+    <div
+      class="contenedor-header contenedor sombra"
+    >
+      <Presupuesto
+      
+      />
+    </div>
+    
   </header>
 
 
-  
+
   </div>
   
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@100&display=swap');
   :root{
     --azul: #3b82f6;
     --blanco: #FFF;
@@ -60,7 +69,24 @@ header h1{
   text-align: center;
 }
 
+.contenedor{
+  width: 90%;
+  max-width: 80rem;
+  margin: auto;
+}
 
+.contenedor-header{
+  margin-top: -5rem;
+  transform: translateY(5rem);
+  padding: 5rem;
+}
+
+.sombra{
+  box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1);
+  background-color: var(--blanco);
+  border-radius: 1.2rem;
+  padding: 5rem;
+}
 
 
 
---
+++ src/components/Presupuesto.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+<template>
+    <form>
+        <div class="campo">
+            <label>
+                Definir Presupuest
+                <input
+                    class="nuevo-presupuestp"
+                    id="nuevo-presupuesto"
+                    placeholder="Registra tu Presupuesto"
+                >
+            </label>
+        </div>
+        <input type="submit" value="Definir Presupuesto"/>
+    </form>
+</template>
+
+<style scoped>
+
+</style>
