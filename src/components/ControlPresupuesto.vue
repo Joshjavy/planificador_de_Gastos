99abe7bf5c8f8325065eed3ff9d992dbe3295e26
Javier Dominguez Galindo <joshjavy@gmail.com>
2024-01-21 21:31:26 -0600

    111. CSS al Panel de Presupuesto

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -30,6 +30,66 @@
     </div>
 </template>
 
-<style lang="scss" scoped>
+<style  scoped>
+    .dos-columnas{
+        display: flex;
+        flex-direction: column;
+    }
+
+    .dos-columnas>:first-child{
+        margin-bottom: 2rem;
+    }
+    @media(min-width: 768px){
+        .dos-columnas{
+        flex-direction:row;
+        gap: 4rem;
+        align-items: center;
+    }
+    .dos-columnas>:first-child{
+        margin-bottom: 0;
+    }
+    }
+    .reset-app{
+        background-color: #db2777;
+        color: var(--blanco);
+        border: none;
+        padding: 1rem;
+        width: 100%;
+        font-weight: 900;
+        text-transform: uppercase;
+        border-radius: 1rem;
+        transition-property: background-color;
+        transition-duration: 300ms;
+    }
+    .reset-app:hover{
+        cursor: pointer;
+        background-color: #be1561;
+    }
+    .contenedor-presupuesto{
+        width: 100%;
+        
+    }
+    .contenedor-presupuesto p{
+        font-size: 2.4rem;
+        text-align: center;
+        color: var(--gris-oscuro);
+    }
+
+    .contenedor-presupuesto span{
+        font-weight: 900;
+        color: var(--azul);
+    }
+
+
+    @media(min-width: 768px){
+        .contenedor-presupuesto p{
+        font-size: 2.4rem;
+        text-align: left;
+
+    }
+    }
+
+
+
 
 </style>
