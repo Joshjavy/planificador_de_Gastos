b005ab4945883ff4731cf756b95c3723e6b2ea74
Javier Dominguez Galindo <joshjavy@gmail.com>
2024-01-24 14:54:37 -0600

    117. Añadiendo el Formulario en el Modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -12,6 +12,43 @@
                 @click="$event => $emit('cerrar-modal')"
             />
         </div>
+
+        <div class="contenedor">
+            <form>
+                <legend>Añadir Gasto</legend>
+
+                <div class="campo">
+                    <label for="nombre">Nombre Gasto:</label>
+                    <input type="text" id="nombre" placeholder="Nombdel del gasto" />
+
+                </div>
+                <div class="campo">
+                    <label for="cantidad">cantidad :</label>
+                    <input type="number" id="cantidad" placeholder="cantidad del gasto" />
+
+                </div>
+                <div class="campo">
+                    <label for="categoria">categoria</label>
+                    <select id="categoria">
+                        <option value="">-- Seleccione --</option>
+                        <option value="ahorro">Ahorro</option>
+                        <option value="comida">comida</option>
+                        <option value="casa">casa</option>
+                        <option value="gastosvarios">gastos varios</option>
+                        <option value="ocio">ocio</option>
+                        <option value="salud">salud</option>
+                        <option value="suscripcciones">Suscripciones</option>
+                    </select>
+                </div>
+                <input type="submit"
+                    value="Registrar Gasto"
+                    class=""
+                    />
+            </form>
+
+        </div>
+
+
     </div>
 </template>
 
