8f90255d389173768c5acfc335e047c663c00824
Javier Dominguez Galindo <joshjavy@gmail.com>
2024-02-04 22:26:18 -0600

    147. Mostrando un Porcentaje de gastos

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -1,4 +1,5 @@
 <script setup>
+    import { computed } from 'vue'
     import CircleProgress from 'vue3-circle-progress'
     import "vue3-circle-progress/dist/circle-progress.css"
     import { formatearCantidad } from '../helpers'
@@ -18,17 +19,24 @@
             required:true
         }
     })
+
+    const porcentaje = computed(()=>{
+        return parseInt(((props.presupuesto - props.disponible) / props.presupuesto) * 100);
+    });
+
 </script>
 <template>
     <div class="dos-columnas">
         <div class="contenedor-grafico">
+            <p class="porcentaje">{{ porcentaje }} %</p>
             <CircleProgress
-                :percent="50"
+                :percent="porcentaje"
                 :size="250"
                 :border-width="30"
                 :border-bg-width="30"
                 fill-color="#3b82f6"
                 empty-color="#e0e0e0"
+                
             />
         </div>
 
@@ -57,6 +65,21 @@
 </template>
 
 <style  scoped>
+    .contenedor-grafico {
+        position: relative;
+    }
+    .porcentaje{
+        position: absolute;
+        margin: auto;
+        top: calc(50% - 1.5rem);;
+        left: 0;
+        right: 0;
+        text-align: center;
+        z-index: 100;
+        font-size: 3rem;
+        font-weight: 900;
+        color: var(--gris-oscuro);
+    }
     .dos-columnas{
         display: flex;
         flex-direction: column;
@@ -115,7 +138,7 @@
     }
     }
 
-
+    
 
 
 </style>
